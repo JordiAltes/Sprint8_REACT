5fc18afe4d6546e50eff7d196b4aff15d8813095
JordiAltes <jordalan8@gmail.com>
2023-03-14 15:56:38 +0100

    header ok

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo_StarWars from "../assets/logo_starWars.png";
+import darthVader from "../assets/darthVader.png";
+const Header = () => {
+  return (
+    <header className="Header">
+      <div className="logoDarthVader">
+        <img src={darthVader} alt="darthVader" className="darthVader" />
+      </div>
+      <div className="Header-logoContainer">
+        <img src={logo_StarWars} alt="logo star wars" className="Header-logo" />
+      </div>
+      <div className="Header-links">
+        <a href="">Log In</a>
+        <span>//</span>
+        <a href="">Sign Up</a>
+      </div>
+    </header>
+  );
+};
+export default Header;
